33ae9761c194dc60fc0efef37abccb57a9d46c9e
Ming Pan <fallspinach@gmail.com>
2022-01-08 22:18:22 -0800

    First commit, adapted from hydro-viewer with basic functionalities

---
+++ app.py
@@ -0,0 +1,207 @@
+import os
+
+import dash
+from dash import dcc
+from dash import html
+from dash.dependencies import ClientsideFunction, Input, Output
+import dash_leaflet as dl
+import dash_leaflet.express as dlx
+from dash_extensions.javascript import Namespace, arrow_function, assign
+
+from datetime import date, datetime, timedelta
+
+# temporary set up
+curr_day   = datetime(2021, 12, 15)
+data_start = datetime(1979, 1, 2)
+data_end   = datetime(2022, 1, 5)
+
+
+## data variable selection dropdown
+var_longnames = ['2-m SM Percentile', 'SWE Percentile']
+var_names = ['smtot_r', 'swe_r']
+
+options_data = [dict(label=var_longnames[i], value=i) for i in range(len(var_longnames))]
+dropdown_data = dcc.Dropdown(options=options_data, value=0, id='dropdown-data', clearable=False)
+
+layer_var_select = html.Div([dropdown_data], id='layer-var-select-container')
+
+
+## CW3E logo
+logo_cw3e = html.A(html.Img(src='assets/cw3e_logo_80.png', title='CW3E Home', sizes='64px, 80px'), href='https://cw3e.ucsd.edu/', id='logo-cw3e')
+
+## B-120 forecast points
+
+point_to_layer = assign('''function(feature, latlng, context){
+    const {min, max, colorscale, circleOptions, colorProp} = context.props.hideout;
+    const csc = chroma.scale(colorscale).domain([min, max]);  // chroma lib to construct colorscale
+    circleOptions.fillColor = csc(feature.properties[colorProp]);  // set color based on color prop.
+    return L.circleMarker(latlng, circleOptions);  // sender a simple circle marker.
+}''')
+
+fcst_points = dl.GeoJSON(url='assets/fnf_points_proj.pbf', format='geobuf', id='points',
+                         options=dict(pointToLayer=point_to_layer), cluster=True, superClusterOptions=dict(radius=5),
+                         hoverStyle=arrow_function(dict(weight=5, color='red', fillColor='red', dashArray='')),
+                         hideout=dict(circleOptions=dict(fillOpacity=1, color='red', weight=2, radius=5), colorscale=['cyan'], colorProp='POINT_Y', min=0, max=100))
+
+
+## B-120 watersheds
+
+watershed_style = dict(weight=2, opacity=1, color='darkblue', fillOpacity=0)
+style_handle = assign('''function(feature, context){
+    const {classes, colorscale, style, colorProp} = context.props.hideout;  // get props from hideout
+    const value = feature.properties[colorProp];  // get value the determines the color
+    for (let i = 0; i < classes.length; ++i) {
+        if (value > classes[i]) {
+            style.fillColor = colorscale[i];  // set the fill color according to the class
+        }
+    }
+    return style;
+}''')
+
+fcst_watersheds = dl.GeoJSON(url='assets/fnf_watershed_proj.pbf', format='geobuf', id='watershed',
+                             options=dict(style=style_handle),
+                             hoverStyle=arrow_function(dict(weight=4, color='brown', dashArray='', fillOpacity=0)),
+                             hideout=dict(colorscale=['darkblue'], classes=[0], style=watershed_style, colorProp='Area_SqMi'))
+
+## CNRFC boundary
+
+cnrfc_style = dict(weight=4, opacity=1, color='gray', fillOpacity=0)
+cnrfc_boundary = dl.GeoJSON(url='assets/cnrfc_bd_degree_wgs84.pbf', format='geobuf', id='cnrfcbd',
+                             options=dict(style=style_handle),
+                             hideout=dict(colorscale=['black'], classes=[0], style=cnrfc_style, colorProp='Area_SqMi'))
+
+## image data layer
+
+cnrfc_domain = [[32, -125], [44, -113]]
+base_url = 'https://cw3e.ucsd.edu/wrf_hydro/cnrfc/imgs/'
+#img_url = base_url + curr_day.strftime('monitor/output/%Y/smtot_r_%Y%m%d%') + var_names[dropdown_forcing.value] + '.png'
+img_url = base_url + curr_day.strftime('monitor/output/%Y/smtot_r_%Y%m%d.png')
+data_map = dl.ImageOverlay(id='data-img', url=img_url, bounds=cnrfc_domain, opacity=0.6)
+
+## all layers
+locator = dl.LocateControl(options={'locateOptions': {'enableHighAccuracy': True}})
+map_cnrfc = dl.Map([dl.TileLayer(), locator,
+                    dl.LayersControl([dl.Overlay(data_map,        id='data-map',        name='Data',        checked=True),
+                                      dl.Overlay(cnrfc_boundary,  id='cnrfc-boundary',  name='CNRFC',       checked=True),
+                                      dl.Overlay(fcst_watersheds, id='fcst-watersheds', name='Watersheds',  checked=True),
+                                      dl.Overlay(fcst_points,     id='fcst-points',     name='Fcst Points', checked=True)])],
+                   center=[38, -119], zoom=6,
+                   style={'width': '100%', 'height': '100%', 'min-height': '800px', 'min-width': '800px', 'margin': '0px', 'display': 'block'})
+
+
+## date picker
+datepicker = html.Div(
+    dcc.DatePickerSingle(
+        id='datepicker',
+        min_date_allowed=data_start.date(),
+        max_date_allowed=data_end.date(),
+        initial_visible_month=data_start.date(),
+        date=curr_day.date()
+    ),
+    id='datepicker-container'
+)
+
+## day slider
+day_marks = {}
+for i in range(31):
+    day_marks[i] = '%d' % (i+1)
+    
+slider_day =  html.Div(
+    dcc.Slider(
+        id='slider-day',
+        min=0,
+        max=30,
+        step=1,
+        marks=day_marks,
+        value=curr_day.day-1
+    ),
+    id='slider-day-container'
+)
+
+## buttons for forward and backward moves
+button_backward_day   = html.Button('<',  id='button-backward-day',   n_clicks=0)
+button_backward_month = html.Button('<<', id='button-backward-month', n_clicks=0)
+button_forward_day    = html.Button('>',  id='button-forward-day',    n_clicks=0)
+button_forward_month  = html.Button('>>', id='button-forward-month',  n_clicks=0)
+
+## figure title
+title_var  = html.Div(var_longnames[0], id='title-var')
+title_date = html.Div(curr_day.strftime(' @ %Y-%m-%d '), id='title-date')
+
+title_zone = html.Div([title_var, title_date], id='title-zone')
+
+## all date controls at the bottom
+bottom_controls = html.Div([datepicker, button_backward_month, button_backward_day, slider_day, button_forward_day, button_forward_month, title_zone],
+                           id='bottom-controls')
+
+# some external things
+external_stylesheets = ['https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css']
+external_scripts     = ['https://cdnjs.cloudflare.com/ajax/libs/chroma-js/2.1.0/chroma.min.js']  # js lib used for colors
+
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets, external_scripts=external_scripts, prevent_initial_callbacks=True)
+
+server = app.server
+
+app.layout = html.Div([
+    map_cnrfc,
+    logo_cw3e,
+    layer_var_select,
+    bottom_controls
+])
+
+## Callbacks from here on
+
+# callback to update data var in the title section
+app.clientside_callback(
+    ClientsideFunction(
+        namespace='clientside',
+        function_name='update_title_var'
+    ),
+    Output('title-var', 'children'),
+    Input(component_id='dropdown-data', component_property='value')
+)
+
+# callback to update data date in the title section
+app.clientside_callback(
+    ClientsideFunction(
+        namespace='clientside',
+        function_name='update_title_date'
+    ),
+    Output('title-date', 'children'),
+    Input('datepicker', 'date')
+)
+
+# callback to update url of image overlay
+app.clientside_callback(
+    ClientsideFunction(
+        namespace='clientside',
+        function_name='update_img_url'
+    ),
+    Output('data-img', 'url'),
+    Input('datepicker', 'date'),
+    Input(component_id='dropdown-data', component_property='value')
+)
+
+
+# callback to update datepicker and slider on button clicks
+app.clientside_callback(
+    ClientsideFunction(
+        namespace='clientside',
+        function_name='update_date'
+    ),
+    Output(component_id='slider-day', component_property='value'),
+    Output('datepicker', 'date'),
+    Input('button-forward-day',  'n_clicks_timestamp'),
+    Input('button-backward-day', 'n_clicks_timestamp'),
+    Input('button-forward-month',   'n_clicks_timestamp'),
+    Input('button-backward-month',  'n_clicks_timestamp'),
+    Input(component_id='slider-day', component_property='value'),
+    Input('datepicker', 'date'),
+    Input('datepicker', 'min_date_allowed'),
+    Input('datepicker', 'max_date_allowed')
+)
+
+
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
---
+++ assets/callbacks.js
@@ -0,0 +1,137 @@
+
+// clientside callbacks for dash to eliminate unnecessary server-client communications and server load
+
+window.dash_clientside = Object.assign({}, window.dash_clientside, {
+    clientside: {
+    
+        // update title variable name
+        update_title_var: function(i_var) {
+            var var_longnames = ['2-m SM Percentile', 'SWE Percentile'];
+            return var_longnames[i_var];
+        },
+        
+        // update title date
+        update_title_date: function(date_value) {
+            return ' @ ' + date_value;
+        },
+        
+        // update title date
+        update_title_hour: function(hour_value) {
+            if (hour_value<10) {
+                return '0' + hour_value + ':00Z';
+            } else {
+                return hour_value + ':00Z';
+            }
+        },
+        
+        // update overlay image url
+        update_img_url: function(date_value, var_value) {
+            var base_url = 'https://cw3e.ucsd.edu/wrf_hydro/cnrfc/imgs/monitor/output/';
+            var var_names = ['smtot_r', 'swe_r'];
+            var d = new Date(date_value);
+            var yyyy = d.getUTCFullYear().toString();
+            var mm = (d.getUTCMonth()+1).toString(); if (mm<10) { mm = '0' + mm; }
+            var dd = d.getUTCDate().toString(); if (dd<10) { dd = '0' + dd; }
+            var new_url = base_url + yyyy + '/' + var_names[var_value] + '_' + yyyy + mm + dd + '.png';
+            return new_url;
+        },
+        
+        // update datepicker and slider on button clicks
+        update_date: function(dfwd_t, dbwd_t, mfwd_t, mbwd_t, d_old, d_old2, d_min, d_max) {
+        
+            var date_old = new Date(d_old2);
+            
+            var date_min =new Date(d_min);
+            var date_max =new Date(d_max);
+            
+            var date_new = new Date();
+            
+            if (dfwd_t==null) { dfwd_t = 0; }
+            if (dbwd_t==null) { dbwd_t = 0; }
+            if (mfwd_t==null) { mfwd_t = 0; }
+            if (mbwd_t==null) { mbwd_t = 0; }
+            
+            if (dfwd_t>dbwd_t && dfwd_t>mfwd_t && dfwd_t>mbwd_t) {
+                // forward-day
+                date_new.setTime(date_old.getTime() + 3600*1000*24);
+            }
+            else if (dbwd_t>dfwd_t && dbwd_t>mfwd_t && dbwd_t>mbwd_t) {
+                // backward-day
+                date_new.setTime(date_old.getTime() - 3600*1000*24);
+            }
+            else if (mfwd_t>dfwd_t && mfwd_t>dbwd_t && mfwd_t>mbwd_t) {
+                // forward-month
+                date_new.setTime(date_old.getTime() + 24*3600*1000*30.4375);
+            }
+            else if (mbwd_t>dfwd_t && mbwd_t>dbwd_t && mbwd_t>mfwd_t) {
+                // backward-day
+                date_new.setTime(date_old.getTime() - 24*3600*1000*30.4375);
+            }
+            else {
+                date_new.setTime(date_old.getTime());
+            }
+            
+            var timenow = Date.now();
+            if (timenow-dfwd_t>100 && timenow-dbwd_t>100 && timenow-mfwd_t>100 && timenow-mbwd_t>100) {
+                date_new.setTime(date_old.getTime());
+            }
+            
+            if (date_new.getTime()<date_min.getTime() || date_new.getTime()>date_max.getTime()) {
+                return [date_old.getUTCDate()-1, date_old.toISOString().split('T')[0]];
+            } else {
+                return [date_new.getUTCDate()-1, date_new.toISOString().split('T')[0]];
+            }
+        },
+        
+        // toggle forcing map
+        toggle_forcing_map: function(selected) {
+            if (selected=='forcing') {
+                return [true, false, {'display': 'inline-block'}];
+            } else {
+                return [false, true, {'display': 'none'}];
+            }
+        },
+        
+        // toggle gauge map
+        toggle_gauge_map: function(layer_selected, gauge_selected) {
+            if (layer_selected=='gauges') {
+                if (gauge_selected=='usgs') {
+                    return [true, false, false];
+                } else {
+                    return [false, true, false];
+                }
+            } else {
+                return [false, false, true];
+            }
+        },
+        
+        // close popup window
+        popup_close: function(n_clicks) {
+            if (n_clicks>0) {
+                return {'display': 'none'};
+            } else {
+                return {'display': 'block'};
+            }
+        },
+        
+        // close popup window
+        popup_open: function(n_clicks_timestamp, usgs_feature, cdec_feature, gauge_selected) {
+            var timenow = Date.now();
+            if (timenow-n_clicks_timestamp<100 || 
+                ((usgs_feature==null || usgs_feature['properties']['STAID']==null) && gauge_selected=='usgs') ||
+                ((cdec_feature==null || cdec_feature['properties']['ID']==null)    && gauge_selected=='cdec')) {
+                return [{'display': 'none'}, 'Station Not Found'];
+            } else {
+                if (gauge_selected=='usgs') { title = usgs_feature['properties']['tooltip']; }
+                if (gauge_selected=='cdec') { title = cdec_feature['properties']['tooltip']; }
+                return [{'display': 'block'}, title];
+            }
+        },
+        
+        // reset click_features
+        reset_click_feature: function(gauge_selected) {
+            return [null, null];
+        }
+        
+    }
+});
