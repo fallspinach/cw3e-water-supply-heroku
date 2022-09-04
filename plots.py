0684f138a7457ade9733cd5c02b919c6d731c4f3
Ming Pan <fallspinach@gmail.com>
2022-09-03 23:37:54 -0700

    no forecast for september

--- plots.py
+++ plots.py
@@ -9,7 +9,7 @@ from datetime import date, datetime, timedelta
 from dateutil.relativedelta import relativedelta
 
 curr_day = datetime.utcnow().date()
-fcst_t1  = curr_day - timedelta(days=curr_day.day-1) - relativedelta(months=1)
+fcst_t1  = curr_day - timedelta(days=curr_day.day-1) - relativedelta(months=2)
 fcst_t2  = fcst_t1 + relativedelta(months=6)-timedelta(days=1)
 #fcst_type = 'fusion'
 fcst_type = 'esp'
